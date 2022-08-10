2ecbf0e1022be61b6621c4ec09c5a3caffaa0b75
stifler97 <stifler@binshops.com>
2022-08-10 10:57:05 -0400

    feat: redirect if category map exists

--- customredirector.php
+++ customredirector.php
@@ -59,160 +59,71 @@ class Customredirector extends Module
      */
     public function install()
     {
-        Configuration::updateValue('CUSTOMREDIRECTOR_LIVE_MODE', false);
-
         return parent::install() &&
-            $this->registerHook('header') &&
-            $this->registerHook('backOfficeHeader');
+            $this->registerHook('actionFrontControllerInitBefore');
     }
 
     public function uninstall()
     {
-        Configuration::deleteByName('CUSTOMREDIRECTOR_LIVE_MODE');
-
         return parent::uninstall();
     }
 
-    /**
-     * Load the configuration form
-     */
-    public function getContent()
+    public function hookActionFrontControllerInitBefore($params)
     {
-        /**
-         * If values have been submitted in the form, process.
-         */
-        if (((bool)Tools::isSubmit('submitCustomredirectorModule')) == true) {
-            $this->postProcess();
+        $controller = $params['controller'];
+        if($controller->php_self !== 'category') {
+            return;
         }
 
-        $this->context->smarty->assign('module_dir', $this->_path);
-
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
-
-        return $output.$this->renderForm();
-    }
-
-    /**
-     * Create the form that will be displayed in the configuration of your module.
-     */
-    protected function renderForm()
-    {
-        $helper = new HelperForm();
-
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->module = $this;
-        $helper->default_form_language = $this->context->language->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
-
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitCustomredirectorModule';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
-
-        return $helper->generateForm(array($this->getConfigForm()));
-    }
-
-    /**
-     * Create the structure of your form.
-     */
-    protected function getConfigForm()
-    {
-        return array(
-            'form' => array(
-                'legend' => array(
-                'title' => $this->l('Settings'),
-                'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->l('Live mode'),
-                        'name' => 'CUSTOMREDIRECTOR_LIVE_MODE',
-                        'is_bool' => true,
-                        'desc' => $this->l('Use this module in live mode'),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => true,
-                                'label' => $this->l('Enabled')
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
-                        ),
-                    ),
-                    array(
-                        'col' => 3,
-                        'type' => 'text',
-                        'prefix' => '<i class="icon icon-envelope"></i>',
-                        'desc' => $this->l('Enter a valid email address'),
-                        'name' => 'CUSTOMREDIRECTOR_ACCOUNT_EMAIL',
-                        'label' => $this->l('Email'),
-                    ),
-                    array(
-                        'type' => 'password',
-                        'name' => 'CUSTOMREDIRECTOR_ACCOUNT_PASSWORD',
-                        'label' => $this->l('Password'),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                ),
-            ),
-        );
-    }
-
-    /**
-     * Set values for the inputs.
-     */
-    protected function getConfigFormValues()
-    {
-        return array(
-            'CUSTOMREDIRECTOR_LIVE_MODE' => Configuration::get('CUSTOMREDIRECTOR_LIVE_MODE', true),
-            'CUSTOMREDIRECTOR_ACCOUNT_EMAIL' => Configuration::get('CUSTOMREDIRECTOR_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-            'CUSTOMREDIRECTOR_ACCOUNT_PASSWORD' => Configuration::get('CUSTOMREDIRECTOR_ACCOUNT_PASSWORD', null),
-        );
-    }
-
-    /**
-     * Save form data.
-     */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
-
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
+        if(!Tools::getIsset('id_category')) {
+            return;
         }
-    }
 
-    /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookBackOfficeHeader()
-    {
-        if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        $id_category = (int) Tools::getValue('id_category');
+
+        try {
+            $categoryMapArray = $this->readCategoryMapFileAndReturnMapArray();
+        } catch (Exception $e) {
+            PrestaShopLogger::addLog("{$this->name} --- {$e->getMessage()}");
+            return;
         }
+
+        if(!array_key_exists($id_category, $categoryMapArray)) {
+            return;
+        }
+
+        $linkToRedirect = $this->context->link->getCategoryLink(
+            $categoryMapArray[$id_category],
+            null,
+            $this->context->language->id
+        );
+
+        Tools::redirect($linkToRedirect);
     }
 
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
+    protected function readCategoryMapFileAndReturnMapArray($fileName = 'categoryMap')
     {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+        $file = "{$this->local_path}{$fileName}.csv";
+
+        if(!file_exists($file)) {
+            throw new Exception("File does not exist: {$file}");
+        }
+
+        $open = fopen($file, "r");
+
+        if(!$open) {
+            throw new Exception("Failed to open file: {$file}");
+        }
+
+        while (($line = fgetcsv($open, 1000, ",")) !== FALSE) 
+        {
+            $id_category_from = $line[0];
+            $id_category_to = $line[1];
+            $categoryMapArray[$id_category_from] = $id_category_to;
+        }
+
+        fclose($open);
+
+        return $categoryMapArray;
     }
 }
